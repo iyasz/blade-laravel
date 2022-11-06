13769614d51c40192f46009e7503301bc8bb9580
Yasz Avellia <zakamaragames@gmail.com>
2022-11-06 08:32:46 +0700

    menambah for loop

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,9 @@
+<?php
+
+
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -46,16 +52,23 @@
     </div>
   </nav>
 
+  {{-- End Navbar  --}}
+
+
   <div class="container-fluid mt-5 text-center">
     <h1>Hallo {{$id}}</h1>
     @if (1 == 1)
     <h1>anime</h1>
     @endif
+    <br>
+
+
+    @for($i = 0; $i < 5; $i++) <p>$i</p>
+      @endfor
 
   </div>
 
 
-  {{-- End Navbar  --}}
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
