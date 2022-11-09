fd5ac3d9ad2dce4b6ac812dcc8a533f23831277a
Yasz Avellia <zakamaragames@gmail.com>
2022-11-09 08:38:18 +0700

    latihan section yield dan extends

--- resources/views/anime.blade.php
+++ resources/views/anime.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Anime</title>
-</head>
-<body>
-    <h1>hallo ! {{$id}}</h1>
-</body>
-</html>
+@extends('layouts.mainlayout')
+
+@section('title', 'anime')
+
+
+@section('content')
+<h1>hai {{ $id }}</h1>
+
+@endsection
